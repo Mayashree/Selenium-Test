62cbb9b67930af120a8f17ec22127a8a5a1c9b45
mayabordoloi <mayabordoloi@localhost>
2024-05-11 16:32:56 -0600

    Added @Listeners({ theinternet.base.TestListener.class }) to BaseTest Class

--- selenium-test/src/main/java/theinternet/base/BaseTest.java
+++ selenium-test/src/main/java/theinternet/base/BaseTest.java
@@ -7,10 +7,11 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
-
+@Listeners({ theinternet.base.TestListener.class })
 public class BaseTest {
 	protected WebDriver driver;
 	protected Logger log;
